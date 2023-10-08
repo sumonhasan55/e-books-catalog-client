ae308ab77160ca4de6958972c01ba7a26781f0d3
sumonhasan55 <wumsumon7577@gmail.com>
2023-10-08 21:29:00 +0600

    modified contact us page

--- src/page/ContactUs.tsx
+++ src/page/ContactUs.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 
@@ -10,7 +11,28 @@ const ContactUs = () => {
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold text-white">Contact Us!</h1>
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+
+                     <div className="">
+                    H M Sumon Hasan 
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-circle btn-ghost btn-xs text-info">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                            </label>
+                            <div tabIndex={0} className="card compact dropdown-content z-[1] shadow bg-base-100 rounded-box w-64">
+                                <div className="card-body">
+                                    <h2 className="card-title">You needed more info?</h2>
+                                    <Link to="https://sumon-hasan-portfolio-nextjs.vercel.app/" className="btn bnt-link">https://sumon-hasan-portfolio-nextjs.vercel.app/</Link>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+
                     </div>
+
+
+
+
+
                     <div className="flex-shrink-0 w-full max-w-sm shadow-2xl card bg-base-100">
                         <form className="card-body">
                             <div className="form-control">
